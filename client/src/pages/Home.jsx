848c43db34e0c3bd3c65330110708a9c29419fb2
JET <JET.jetsada53@hotmail.com>
2026-01-17 16:08:28 +0700

    fix: remove 'local' text from homepage hero

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -78,7 +78,7 @@ const Home = () => {
                             className="text-left"
                         >
                             <h1 className="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
-                                <span className="block">สอบบรรจุข้าราชการท้องถิ่น</span>
+                                <span className="block">สอบบรรจุข้าราชการ</span>
                                 <span className="block text-primary">ติดชัวร์!</span>
                             </h1>
                             <p className="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
