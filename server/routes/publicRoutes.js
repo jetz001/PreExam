f9488dbd9c487008c61deccf2f9c33ba243554a4
JET <JET.jetsada53@hotmail.com>
2026-02-19 12:00:15 +0700

    fix: Add optionalAuthMiddleware to public log route

--- server/routes/publicRoutes.js
+++ server/routes/publicRoutes.js
@@ -2,10 +2,11 @@
 const express = require('express');
 const router = express.Router();
 const publicController = require('../controllers/publicController');
+const { optionalAuthMiddleware } = require('../middleware/authMiddleware');
 
 
 router.get('/stats', publicController.getLandingStats);
 router.get('/settings', publicController.getSystemSettings);
-router.post('/log', publicController.logFrontendActivity);
+router.post('/log', optionalAuthMiddleware, publicController.logFrontendActivity);
 
 module.exports = router;
